a0e2ce4b7197968f15fcf08a10bc940537db127b
fjhanevo <fredrik.hanevold@gmail.com>
2025-05-09 18:27:54 +0200

    added randomize_string

--- exercise4/src/utilities.rs
+++ exercise4/src/utilities.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use rand::{random, Rng};
 
 pub fn incerement_by_value_num_times(mut start_value: u32, increment: u32, num_times: u32) -> u32
 {
@@ -35,4 +36,22 @@ impl fmt::Display for Student {
 pub fn print_student(student: &Student)
 {
     println!("{}", student);
+}
+
+fn random_with_limits(lower_lim: u32, upper_lim: u32) -> u32
+{
+    let mut rng = rand::thread_rng();
+    rng.gen_range(lower_lim..=upper_lim)
+}
+
+pub fn randomize_string(n: u32, lower_lim: char, upper_lim: char) -> String
+{
+    let mut return_string = String::new();
+    for _ in 0..n {
+        let rand_u32 = random_with_limits(lower_lim as u32, upper_lim as u32);
+        if let Some(c) = char::from_u32(rand_u32) { 
+            return_string.push(c);
+        }
+    } 
+    return_string
 }

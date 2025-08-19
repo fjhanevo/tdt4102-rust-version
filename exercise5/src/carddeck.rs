28857d9f09e1b9889e8214b3de32dcba012b5bc7
fjhanevo <fredrik.hanevold@gmail.com>
2025-08-19 12:43:02 +0200

    added print, shuffle and draw_card fns

--- exercise5/src/carddeck.rs
+++ exercise5/src/carddeck.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use crate::card::{Card, Suit, Rank};
 
 pub struct CardDeck {
@@ -18,7 +19,7 @@ impl CardDeck {
     }
 
     // Manual implementation of swap for learning
-    pub fn swap(&mut self, a: usize, b: usize) {
+    fn swap(&mut self, a: usize, b: usize) {
         // temporarily remmoves one element
         let temp = self.cards.remove(a);
 
@@ -31,5 +32,25 @@ impl CardDeck {
 
         self.cards.insert(a, replacement);
         self.cards.insert(b, temp);
+    }
+
+    pub fn print(&self) {
+        for c in self.cards.iter() {
+            println!("{}", c.to_string());
+        }
+    }
+
+    pub fn shuffle(&mut self) {
+        let mut rng = rand::rng();
+        let len = self.cards.len();
+        for _ in 0..len {
+            let a = rng.random_range(0..len-1);
+            let b = rng.random_range(0..len-1);
+            self.swap(a,b);
+        }
+    }
+
+    pub fn draw_card(&mut self) -> Option<Card> {
+        Some(self.cards.pop().expect("Deck is empty."))
     }
 }

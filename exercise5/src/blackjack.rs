9b587a5ce039a2e1a699f23a44fa8f00fe5f74dd
fjhanevo <fredrik.hanevold@gmail.com>
2025-08-20 12:52:15 +0200

    added new() and play_game()

--- exercise5/src/blackjack.rs
+++ exercise5/src/blackjack.rs
@@ -13,15 +13,25 @@ pub struct Blackjack {
 }
 
 impl Blackjack {
+    // constructor
+    pub fn new() -> Self {
+        Blackjack {
+            deck: CardDeck::new(),
+            player_hand: Vec::new(),
+            dealer_hand: Vec::new(),
+            player_hand_sum: 0,
+            dealer_hand_sum: 0,
+        } 
+    }
     fn is_ace(c: &Card) -> bool {
         c.get_rank() == Rank::Ace
     }
 
-    pub fn get_card_value(c: &Card) -> u8 {
+    fn get_card_value(c: &Card) -> u8 {
         c.get_rank().blackjack_values()
     }
 
-    pub fn get_hand_score(&self, cards: &[Card]) -> u8 {
+    fn get_hand_score(&self, cards: &[Card]) -> u8 {
         let mut sum = 0;
         let mut aces = 0;
 
@@ -43,7 +53,7 @@ impl Blackjack {
         sum
     }
 
-    pub fn ask_player_draw_card() -> bool {
+    fn ask_player_draw_card() -> bool {
         let mut input = String::new();
         println!("Do you want to draw a card? [y/n]");
 
@@ -55,25 +65,103 @@ impl Blackjack {
             return true
         }
         false
-
-
-
     }
 
-    pub fn draw_player_card(&mut self) {
+    fn draw_player_card(&mut self) {
         if let Some(card) = self.deck.draw_card() {
             self.player_hand.push(card);
             println!("You drew {}", card.to_string());
+            self.player_hand_sum += Self::get_card_value(&card);
         } else {
             println!("Deck is empty!");
         }
     }
 
-    pub fn draw_dealer_card(&mut self) {
+    fn draw_dealer_card(&mut self) {
         if let Some(card) = self.deck.draw_card() {
             self.dealer_hand.push(card);
+            self.dealer_hand_sum += Self::get_card_value(&card);
         } else {
             println!("Deck is empty!");
+        }
+    }
+
+    fn start_round(&mut self) {
+        if self.deck.cards.len() < 16 {
+            self.deck = CardDeck::new();
+            self.deck.shuffle();
+        }
+        self.player_hand_sum = 0;
+        self.dealer_hand_sum = 0;
+
+        // empty both hands
+        self.player_hand.clear();
+        self.dealer_hand.clear();
+
+        self.draw_player_card();
+        self.draw_dealer_card();
+        self.draw_player_card();
+        self.draw_dealer_card();
+    }
+
+    pub fn play_game(&mut self) {
+        println!("Welcome to Blackjack!");
+        let mut win: bool = false;
+
+        // shuffle twice for fun
+        self.deck.shuffle();
+        self.deck.shuffle();
+
+        loop {
+            self.start_round();
+
+            println!("Player hand sum is: {}", self.player_hand_sum);
+            if let Some(card) = self.dealer_hand.get(0) {
+                println!("The dealer's first card is: {}", card.to_string());
+            }
+
+            while self.player_hand_sum < 21 && Self::ask_player_draw_card() {
+                println!("Player draws a card.");
+                self.draw_player_card();
+                println!("Player hand sum is: {}", self.player_hand_sum);
+            }
+
+            while self.dealer_hand_sum < 17 {
+                println!("Dealer draws a card.");
+                self.draw_dealer_card();
+            }
+
+            println!("**********");
+            println!("Player's cards: ");
+            for card in self.player_hand.iter() {
+                println!("{}", card.to_string());
+            }
+            println!("Dealer's hand sum: {}", self.dealer_hand_sum);
+
+            if self.player_hand_sum > 21 {
+                println!("Player burst!");
+            } else if self.dealer_hand_sum > 21 {
+                println!("Dealer burst!");
+                win = true;
+            } else if self.player_hand_sum == 21 && self.player_hand.len() == 2 {
+                println!("Player got Blackjack!\n");
+                win = true;
+            } else if self.dealer_hand_sum == 21 && self.dealer_hand.len() == 2 {
+                println!("Dealer got Blackjack!\n");
+            } else if self.player_hand_sum <= 21 && self.player_hand_sum > self.dealer_hand_sum {
+                println!("Player beat dealer!\n");
+                win = true;
+            } else if self.dealer_hand_sum <= 21 && self.dealer_hand_sum > self.player_hand_sum {
+                println!("Dealer beat player!\n");
+            }
+
+            if win {
+                println!("You won!");
+            } else {
+                println!("You lost!");
+            }
+            break;
+
         }
     }
 }

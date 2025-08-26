c53696f0983057006648cd8801bf8b751a0918a6
fjhanevo <fredrik.hanevold@gmail.com>
2025-08-26 22:00:15 +0200

    added add_line_numbers to main

--- exercise6/src/main.rs
+++ exercise6/src/main.rs
@@ -1,5 +1,8 @@
 mod file_utils;
-use file_utils::write_user_input_to_file;
+use file_utils::{write_user_input_to_file, add_line_numbers };
 fn main() {
-    write_user_input_to_file();
+    let file = "tmp.txt";
+    write_user_input_to_file(&file.to_owned());
+    add_line_numbers(&file.to_owned());
+
 }

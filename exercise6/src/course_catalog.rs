f3e56cce87378aa984963bf0ec2ee0535ce71acf
fjhanevo <fredrik.hanevold@gmail.com>
2025-08-27 17:11:08 +0200

    overloaded Display for CourseCatalog

--- exercise6/src/course_catalog.rs
+++ exercise6/src/course_catalog.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::fmt;
 
 #[derive(Debug)]
 pub struct CourseCatalog {
@@ -6,11 +7,31 @@ pub struct CourseCatalog {
 }
 
 impl CourseCatalog {
+    pub fn new() -> Self {
+        CourseCatalog {
+            courses: HashMap::new(),
+        }
+    }
     pub fn add_course(&mut self, key: String, value: String) {
         self.courses.insert(key, value);
     }
     
     pub fn remove_course(&mut self, key: String) {
         self.courses.remove(&key);
+    }
+
+    pub fn get_course(&self, key: &str) -> Option<&String>{
+        self.courses.get(key)
+    }
+}
+
+// operator overload
+impl fmt::Display for CourseCatalog {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "--- Course Catalog ---")?;
+        for (key, value) in self.courses.iter() {
+            writeln!(f, "{}: {}", key, value)?;
+        }
+        Ok(())
     }
 }

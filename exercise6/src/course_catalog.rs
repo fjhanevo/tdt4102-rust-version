2e8c7282ec726560acc3be564ab1add1e9613c51
fjhanevo <fredrik.hanevold@gmail.com>
2025-08-30 16:04:43 +0200

    added save_to_file

--- exercise6/src/course_catalog.rs
+++ exercise6/src/course_catalog.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 use std::fmt;
+use std::fs::OpenOptions;
+use std::io::Write;
 
 #[derive(Debug)]
 pub struct CourseCatalog {
@@ -22,6 +24,25 @@ impl CourseCatalog {
 
     pub fn get_course(&self, key: &str) -> Option<&String>{
         self.courses.get(key)
+    }
+
+    pub fn save_to_file(&self, file_path: &str) {
+        if self.courses.is_empty() {
+            println!("No courses to save!");
+            return;
+        }
+
+        let mut save_file = OpenOptions::new()
+            .create(true)       // create if it doesn't already exist
+            .write(true)        // open in "w" mode
+            .append(true)       // append instead of overwriting
+            .open(file_path)    
+            .expect("Unable to open file!");
+
+            
+        for (key, value) in self.courses.iter() {
+            writeln!(save_file, "{}: {}", key, value).expect("Failed to write data!");
+        } 
     }
 }
 
